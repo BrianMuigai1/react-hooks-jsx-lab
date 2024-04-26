30c9d85782f5ed434a810551815bdb66c7bfa69f
Brian <brian.m.muigai@gmail.com>
2024-04-26 16:16:13 +0300

    feat:completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { image } from "../data/data";
+import imageVariable from "./pexels-valeriiamiller-20801061.jpg"; 
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      About
+      <h2>Content of About Me</h2>
+      <p>I am a small footed short luo darkskin man</p>
+      <img src={imageVariable} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,17 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  // Update the JSX being returned
+  const h1Style = {
+    color: 'firebrick'
+  };
+
+  return (
+    <div id="home">
+      Home
+      <h1 style={h1Style}>{`${name} is a Web Developer from ${city}`}</h1>
+    </div>
+  );
 }
 
 export default Home;
